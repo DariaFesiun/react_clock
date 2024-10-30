9186f9904e1380144f07b3434e0b56f8e46522cb
daradanilova <lemma1102@gmail.com>
2024-10-30 14:19:54 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,61 @@
 import React from 'react';
 import './App.scss';
-import { Clock } from './components/clock';
+import { Clock } from './Clock';
+
+function getRandomName(): string {
+  const value = Date.now().toString().slice(-4);
+
+  return `Clock-${value}`;
+}
 
 type State = {
   hasClock: boolean;
   clockName: string;
 };
 
-export class App extends React.Component<State> {
-  state: State = {
+export class App extends React.Component<{}, State> {
+  state = {
     hasClock: true,
     clockName: 'Clock-0',
   };
 
-  timerId2 = 0;
+  timerId = 0;
 
-  handleClockHide = (event: MouseEvent) => {
+  componentDidMount(): void {
+    this.startClockNameUpdate();
+    document.addEventListener('contextmenu', this.hideClock);
+    document.addEventListener('click', this.showClock);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timerId);
+    document.removeEventListener('contextmenu', this.hideClock);
+    document.removeEventListener('click', this.showClock);
+  }
+
+  hideClock = (event: MouseEvent) => {
     event.preventDefault();
     this.setState({ hasClock: false });
   };
 
-  handleClockShow = (event: MouseEvent) => {
-    event.preventDefault();
+  showClock = () => {
     this.setState({ hasClock: true });
   };
 
-  getRandomName(): string {
-    const value = Date.now().toString().slice(-4);
-
-    return `Clock-${value}`;
-  }
-
-  componentDidMount(): void {
-    document.addEventListener('contextmenu', this.handleClockHide);
-    document.addEventListener('click', this.handleClockShow);
-    this.timerId2 = window.setInterval(() => {
-      const currentName = this.getRandomName();
-
-      this.setState({ clockName: currentName });
+  startClockNameUpdate = () => {
+    this.timerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
     }, 3300);
-  }
-
-  componentDidUpdate(prevState: Readonly<State>): void {
-    if (prevState.clockName !== this.state.clockName && this.state.hasClock) {
-      // eslint-disable-next-line no-console
-      console.warn(
-        `Renamed from ${prevState.clockName} to ${this.state.clockName}`,
-      );
-    }
-  }
-
-  componentWillUnmount(): void {
-    window.clearInterval(this.timerId2);
-    document.removeEventListener('contextmenu', this.handleClockHide);
-  }
+  };
 
   render() {
-    // const { hasClock, clockName } = this.state;
+    const { hasClock, clockName } = this.state;
 
     return (
       <div className="App">
         <h1>React clock</h1>
 
-        {this.state.hasClock && <Clock name={this.state.clockName} />}
+        {hasClock && <Clock name={clockName} />}
       </div>
     );
   }
